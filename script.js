0fad56a85d42a929d48c0449c81d27a6ccfb331d
svinkova <msvinkova@gmail.com>
2023-06-22 13:33:19 +0200

    Upraveno sticky menu logo a přidán první script na logo v menu

---
+++ script.js
@@ -0,0 +1,16 @@
+window.addEventListener('scroll', function() {
+    let menu = document.querySelector('nav');
+    let logo = document.querySelector('.sticky-logo-text');
+    let scrollPosition = window.scrollY;
+  
+    if (scrollPosition > 165) {
+      menu.classList.add('sticky');
+      logo.classList.add('sticky');
+    } else {
+      menu.classList.remove('sticky');
+      logo.classList.remove('sticky');
+    }
+  });
+
+  
+  
